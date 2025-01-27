1ffb4bb3fe014826edbc26d1c9459aec10136349
Vinicius <boringmkt@gmail.com>
2025-01-27 07:38:20 -0300

    :zap: Add page Cadastro, Login and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
+import Home from './pages/home/Home'
+import Login from './pages/login/Login'
+import Cadastro from './pages/cadastro/Cadastro'
 
 function App() {
-  
+
   return (
     <>
-      <Navbar />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom"
+
 function Navbar() {
     return (
         <>
             <div className='w-full flex justify-center py-4
             			   bg-indigo-900 text-white'>
-            
+
                 <div className="container flex justify-between text-lg">
-                    Blog Pessoal
+                    <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
 
                     <div className='flex gap-4'>
                         Postagens
