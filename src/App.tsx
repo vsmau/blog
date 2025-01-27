9122bae43c2e45c791de76e1e878ea46701aebf9
Vinicius <boringmkt@gmail.com>
2025-01-27 06:13:57 -0300

    :zap: add Navbar and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import Home from './pages/home/Home'
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
 
 function App() {
-
+  
   return (
     <>
-      <Home/>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
