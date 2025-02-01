6b3be22cfae25a64913dd1607dd58b98078762b4
Vinicius <boringmkt@gmail.com>
2025-02-01 19:57:47 -0300

    :zap: Add Profile Components

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import DeletarTema from './components/temas/deletartema/DeletarTema'
 import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
 import FormPostagem from './components/postagens/formpostagem/FormPostagem'
 import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import Perfil from './pages/perfil/Perfil'
 
 function App() {
 
@@ -34,6 +35,7 @@ function App() {
               <Route path="/cadastrarpostagem" element={<FormPostagem />} />
               <Route path="/editarpostagem/:id" element={<FormPostagem />} />
               <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
