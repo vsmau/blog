bb897b80ed84c59cf53c24105ed443aecc8c1ac9
Vinicius <boringmkt@gmail.com>
2025-02-01 23:18:32 -0300

    :wrench: Fixed Service LN4

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const api = axios.create({
-    baseURL: import.meta.env.VITA_API_URL
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async (url: string, dados: Object, setDados: Function) => {
